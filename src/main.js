be623a80acd421ce51b26f558c7687bb073d82df
Guillaume Berard <berardgui@gmail.com>
2017-02-19 15:28:37 -0500

    Main loop music

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 
 var g_game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-template', {preload: preload, create: create});
+var g_music;
 
 function preload() {
     // load resources
+    g_game.load.audio('main_loop', 'res/music/main_loop.mp3');
 }
 
 function create() {
     // start game
     g_game.stage.backgroundColor = '#2d2d2d';
+
+    g_music = g_game.add.audio('main_loop');
+    g_game.sound.setDecodedCallback(g_music, on_music_decoded, this);
+}
+
+function on_music_decoded() {
+    g_music.loopFull(0.6);
 }
