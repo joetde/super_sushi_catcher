85a5c00c8dd2105c37a2f9acbd6bd414d90ecc1c
Guillaume Berard <berardgui@gmail.com>
2017-02-19 15:11:26 -0500

    Grey screen!

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+var g_game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-template', {preload: preload, create: create});
+
+function preload() {
+    // load resources
+}
+
+function create() {
+    // start game
+    g_game.stage.backgroundColor = '#2d2d2d';
+}
